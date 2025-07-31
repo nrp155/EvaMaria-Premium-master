8f1b8945769c386b10508d492717a9fb800bc1d1
nrp155 <84077310+nrp155@users.noreply.github.com>
2025-07-31 10:57:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://subscenelkbatch:subscenelkbatch@cluster0.bbbnzwo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://batchfile2:batchfile2@cluster2.wb6wirh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster2")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
